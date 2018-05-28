3fb688cdb6a3b0abaa1361295066b7e3679cbb21
Khoa Bui <thomasbui93@gmail.com>
2018-05-28 23:01:49 +0100

    add active functional

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,16 @@ import Link from 'gatsby-link'
 export default () => {
   return <div className="header__container">
     <div className="header__menu">
-      <div className="header__item">
+      <div className={`header__item ${!isActive('/tags') ? 'is--active': '' }`}>
         <Link to='/'>Today I've learnt</Link>
       </div>
-      <div className="header__item">
+      <div className={`header__item ${isActive('/tags') ? 'is--active': '' }`}>
         <Link to='/tags'>Topic</Link>
       </div>
     </div>
   </div>
+}
+
+export const isActive = (url) => {
+  return window.location.pathname.indexOf(url) > -1;
 }
