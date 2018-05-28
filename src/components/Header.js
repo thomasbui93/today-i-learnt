930b4098f61e6af1acd93189b01c473b1da624ee
Khoa Bui <thomasbui93@gmail.com>
2018-05-28 23:24:32 +0100

    add active style for menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-export default () => {
-  return <div className="header__container">
-    <div className="header__menu">
-      <div className={`header__item ${!isActive('/tags') ? 'is--active': '' }`}>
-        <Link to='/'>Today I've learnt</Link>
-      </div>
-      <div className={`header__item ${isActive('/tags') ? 'is--active': '' }`}>
-        <Link to='/tags'>Topic</Link>
-      </div>
+export default class Header extends React.Component {
+  render() {
+    return <div className="header__container">
+        <div className="header__menu">
+          <div className="header__item">
+            <Link 
+              to='/' 
+              >Today I've learnt</Link>
+          </div>
+          <div className="header__item">
+            <Link to='/tags' activeStyle={{
+                color: '#e74c3c'
+              }}>Topic</Link>
+          </div>
+        </div>
     </div>
-  </div>
-}
-
-export const isActive = (url) => {
-  return window.location.pathname.indexOf(url) > -1;
+  }
 }
